0f7c2a23cf89e56a28b58dedd06c9cf1b08bc68a
majakwani <anasjakwani72@gmail.com>
2022-08-29 11:53:25 +0500

    Created a class that initializes object for Payments

--- Payment.js
+++ Payment.js
@@ -1,15 +1,10 @@
-"use strict";
-exports.__esModule = true;
-exports.Payment = void 0;
-var Payment = /** @class */ (function () {
-    function Payment(client, details, amount) {
-        this.client = client;
+export class Payment {
+    constructor(recipient, details, amount) {
+        this.recipient = recipient;
         this.details = details;
         this.amount = amount;
     }
-    Payment.prototype.format = function () {
-        return "".concat(this.client, " is owed $").concat(this.amount, " for ").concat(this.details);
-    };
-    return Payment;
-}());
-exports.Payment = Payment;
+    format() {
+        return `${this.recipient} is owed $${this.amount} for ${this.details}`;
+    }
+}
